1f12dd34c7f7f95ef5bad0819eecf22c9fa0e090
Paul Boon <gvr37.p.l.boon@gmail.com>
2018-03-22 23:36:10 +0100

    p

--- main.ts
+++ main.ts
@@ -3,14 +3,11 @@
 
 var redblackTree = new BinaryTree.RedBlackTree()
 
-var keys:number[] = [0,1,2,3,4,5,6,7,8,9]
-
-for(var key of keys){
+for (let key = 0; key < 100; key++) {
     redblackTree.insert(key,0)
     console.clear()
     for(var arr of redblackTree.toArray()){
         console.log(arr.map((node) => `${node.key},${node.isRed}`))
     }
 }
-
 // redblackTree
